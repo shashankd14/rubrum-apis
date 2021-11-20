25b36e7bfa1e180bf222e36f11dc078535b71f9d
mohak <mohakrulesenator@gmail.com>
2021-11-20 14:04:58 +0530

    minor fix

--- src/main/java/com/steel/product/application/dto/instruction/SlitInstructionDeleteRequest.java
+++ src/main/java/com/steel/product/application/dto/instruction/SlitInstructionDeleteRequest.java
@@ -2,11 +2,13 @@ package com.steel.product.application.dto.instruction;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class SlitInstructionDeleteRequest {
 
     private Long partId;
