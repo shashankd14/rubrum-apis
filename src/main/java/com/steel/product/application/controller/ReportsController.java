20c2169cccc132611c5fa61e9a8d757d9ab12c12
Kanakadri <kanakadri.rayi@techurate.com>
2022-10-31 17:50:36 +0530

    yml deleted

--- src/main/java/com/steel/product/application/controller/ReportsController.java
+++ src/main/java/com/steel/product/application/controller/ReportsController.java
@@ -1,11 +1,22 @@
 package com.steel.product.application.controller;
 
+import com.steel.product.application.dao.PartyDetailsRepository;
 import com.steel.product.application.dto.report.StockReportRequest;
+import com.steel.product.application.entity.Party;
+import com.steel.product.application.service.MailSender;
 import com.steel.product.application.service.ReportsService;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,7 +26,14 @@ import org.springframework.web.bind.annotation.*;
 public class ReportsController {
 
     private final ReportsService reportsService;
+    
+	@Autowired
+	@Qualifier("apiEmailReports")
+	public MailSender mailSender;
 
+	@Autowired
+	PartyDetailsRepository partyRepo;
+	
     @Autowired
     public ReportsController(ReportsService reportsService) {
         this.reportsService = reportsService;
@@ -26,9 +44,21 @@ public class ReportsController {
         return reportsService.generateAndMailStockReport(stockReportRequest);
     }
 
-//    @GetMapping("test")
-//    public void importCSV() throws IOException, ParseException {
-//        reportsService.uploadCSV();
-//    }
+	@GetMapping("/sendReportEmail/{partyId}")
+	public void sendNotificationAlert(@PathVariable("partyId") int partyId) {
+
+		Calendar cal = Calendar.getInstance();
+		Date date = cal.getTime();
+		DateFormat dateFormat = new SimpleDateFormat("dd-MMM-yyyy");
+		String strDate = dateFormat.format(date);
+
+		List<Party> partyList = partyRepo.findAll();
+		for (Party party : partyList) {
+			if (party.getEmail1() != null && party.getEmail1().length() > 0 && party.getnPartyId() == partyId) {
+				mailSender.sendMail(party.getPartyName(), party.getEmail1(), party.getEmail2(), strDate, party.getnPartyId());
+			}
+		}
+
+	}
 
 }
